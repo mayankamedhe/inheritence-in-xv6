f9828b93485b578abf2ac31bed1e4c523fda66dc
mayankamedhe <mayankaanilmedhe@gmail.com>
2019-09-17 01:53:51 +0530

    Add files via upload

---
+++ sysproc.c
@@ -0,0 +1,121 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int
+sys_getppid(void)
+{
+  return (myproc()->parent)->pid;
+}
+
+//enum signal_type { SIGNAL_PAUSE =0, SIGNAL_KILL, SIGNAL_CONTINUE };
+
+int
+sys_signal_process(void)
+{
+
+	int pid;
+	int type;
+	argint(0, &pid);
+	argint(1, &type);
+
+	return signal_process_h(pid, (enum signal_type)type);
+}
+
+int
+sys_pause(void)
+{
+	int pid;
+	int duration;
+	argint(0, &pid);
+	argint(1, &duration);
+	return pause_h(pid, duration);
+}
---
+++ types.h
@@ -0,0 +1,6 @@
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+typedef unsigned char  uchar;
+typedef uint pde_t;
+
+enum signal_type { SIGNAL_CONTINUE =0, SIGNAL_KILL, SIGNAL_PAUSE };
